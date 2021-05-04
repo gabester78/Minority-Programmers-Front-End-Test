7ba973b271de7301c59c9b94262b54996202d555
Gabriel Romero <gabester78@gmail.com>
2021-05-04 18:18:57 -0500

    App created and navigation bar done.

---
+++ mp/src/App.js
@@ -0,0 +1,12 @@
+import './scss/App.scss';
+import Navbar from "./components/nav_bar.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
