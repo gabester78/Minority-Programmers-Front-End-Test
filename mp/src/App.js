e94a90e1d5b50c8796132c8fd6724ea4272d49c6
Gabriel Romero <gabester78@gmail.com>
2021-05-05 21:32:19 -0500

    Home component done.

--- mp/src/App.js
+++ mp/src/App.js
@@ -1,10 +1,17 @@
 import './scss/App.scss';
-import Navbar from "./components/nav_bar.js"
+import Navbar from "./components/nav_bar.js";
+import Home from "./components/home.js";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* <Route path="/incubator" component={Incubator} />
+        <Route path="/learn" component={Learn} /> */}
+      </Switch>
     </div>
   );
 }
---
+++ mp/src/components/home.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Hero from "../assets/Hero.jpg";
+import Banner from "../assets/Banner.jpg";
+import Classroom from "../assets/Classroom.jpg";
+
+const Home = () => {
+    return (
+        <main>
+            <section className="hero">
+                <img src={Hero} alt="Student typing on a laptop" />
+            </section>                
+            <button className="BTNorange">Learn More</button>
+            <section className="network" style={{ backgroundImage: `url(${Banner})`}}>
+                <div>
+                    <h1 className="network_heading">An international network of programmers</h1>
+                </div>
+                <div className="network_details">                
+                    <h6>Teaching, mentoring, learning STEM education</h6>
+                    <p>Learn to code and grow your skills</p>
+                    <button className="BTNblue">Start Today</button>
+                </div>
+            </section>
+            <section className="events" style={{ backgroundImage: `url(${Classroom})`}}>
+                <div className="events_details">                
+                    <p>
+                        We aim to help marginalized communities access opportunities through events, incubation, education, 
+                        and capital.
+                    </p>
+                    <p>
+                        Minority Programmers is an international network of community activists, students, professionals, and 
+                        developers, unifying together to develop socially impactful software solutions and empower underrepresented 
+                        communities through STEM education.
+                    </p>
+                </div>
+                <div className="events_heading">
+                    <h2>Minority Programmers mission</h2> <h2>is to bring diversity through STEM education and innovation.</h2>
+                    <button className="BTNorange2">Learn More</button>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default Home;
