d2ee47bb7032394762c3789a34a2ca4a58607623
Gabriel Romero <gabester78@gmail.com>
2021-05-11 00:23:52 -0500

    Added simple footer component.

--- mp/src/App.js
+++ mp/src/App.js
@@ -9,6 +9,7 @@ import Activity from "./components/activity.js";
 import Activity_white from "./components/activity_white.js";
 import Activity1 from "./components/activity1.js";
 import Activity1_white from "./components/activity1_white.js";
+import Footer from "./components/footer.js";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
         <Route path="/activity1" component={Activity1} />
         <Route path="/activity1_white" component={Activity1_white} />
       </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ mp/src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+        <footer>
+            <p>©2021 Minority Programmers</p>
+        </footer>
+  );
+};
+
+export default Footer;
