6c8288dd37744056a435dc797922264823bd9d07
Gabriel Romero <gabester78@gmail.com>
2021-10-16 12:23:57 -0500

    Homepage mostly refactored for responsiveness.

--- mp/src/components/home.js
+++ mp/src/components/home.js
@@ -2,55 +2,63 @@ import React from "react";
 import Hero from "../assets/Hero.jpg";
 import Banner from "../assets/Banner.jpg";
 import Classroom from "../assets/Classroom.jpg";
-import NavBar from "./nav_bar.js"
+import NavBar from "./nav_bar.js";
 import Banner2 from "../assets/Banner2.svg";
 import Banner3 from "../assets/Banner3.svg";
 
 const Home = () => {
-    return (
-        <main>
-            <NavBar />
-            <section className="hero">
-                <img src={Hero} alt="Student typing on a laptop" />
-            </section>                
-            <button className="BTNorange">Learn More</button>
-            <section className="network" style={{ backgroundImage: `url(${Banner})`, backgroundRepeat: 'no-repeat'}}>
-                <div>
-                    <h1 className="network_heading">An international network of programmers</h1>
-                </div>
-                <div className="network_details">                
-                    <h6>Teaching, mentoring, learning STEM education</h6>
-                    <p>Learn to code and grow your skills</p>
-                    <button className="BTNblue">Start Today</button>
-                </div>
-            </section>
-            <section className="events" style={{ backgroundImage: `url(${Classroom})`, backgroundRepeat: 'no-repeat'}}>
-                <div className="events_details">                
-                    <p>
-                        We aim to help marginalized communities access opportunities through events, incubation, education, 
-                        and capital.
-                    </p>
-                    <p>
-                        Minority Programmers is an international network of community activists, students, professionals, and 
-                        developers, unifying together to develop socially impactful software solutions and empower underrepresented 
-                        communities through STEM education.
-                    </p>
-                    <button className="BTNorange2">Learn More</button>
-                </div>
-                <div className="events_heading">
-                    <h2>Minority Programmers mission</h2>
-                    <h2>is to bring diversity through STEM education and innovation.</h2>
-                </div>
-            </section>
-            <section className="principles" style={{ backgroundImage: `url(${Banner2})`, backgroundRepeat: 'no-repeat'}}>
-                <h2>Our Core Principles</h2>
-                <p>They're using our own satellites against us. And the clock is ticking. Eventually, you do plan to have dinosaurs on your dinosaur tour, right? Must go faster... go, go, go, go, go! Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should.</p>
-            </section>
-            <section className="services">
-                <img src={Banner3} alt="Graphic with details about services" />
-            </section>
-        </main>
-    )
-}
+  return (
+    <main>
+      <NavBar />
+      <article className="hero">
+        <img src={Hero} alt="Student typing on a laptop" />
+        <button className="BTNorange">Learn More</button>
+      </article>
+
+      <article className="network">
+        <h1>An international network of programmers</h1>
+
+        <section className="network_details">
+          <p>
+            Teaching, mentoring, learning STEM education. Learn to code and grow
+            your skills.
+          </p>
+          <button className="BTNblue">Start Today</button>
+        </section>
+      </article>
+
+      <article className="events">
+        <section className="events_details">
+          <p>
+            We aim to help marginalized communities access opportunities through
+            events, incubation, education, and capital.
+          </p>
+          <p>
+            Minority Programmers is an international network of community
+            activists, students, professionals, and developers, unifying
+            together to develop socially impactful software solutions and
+            empower underrepresented communities through STEM education.
+          </p>
+          <button className="BTNorange">Learn More</button>
+        </section>
+        <h2 className="events_heading">
+          Minority Programmers mission is to bring diversity through STEM
+          education and innovation.
+        </h2>
+      </article>
+
+      <article className="principles">
+        <h3>Our Core Principles</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          justo libero, elementum non volutpat quis, iaculis ut nisi. Sed sit
+          amet ligula eget dolor maximus finibus et ac magna.
+        </p>
+      </article>
+
+      {/* <img src={Banner3} alt="Graphic with details about services" /> */}
+    </main>
+  );
+};
 
 export default Home;
--- mp/src/components/nav_bar.js
+++ mp/src/components/nav_bar.js
@@ -1,36 +1,72 @@
 import React from "react";
 import Mplogo from "../assets/mplogo.png";
 import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter, faFacebookSquare, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitterSquare,
+  faFacebookSquare,
+  faLinkedin,
+  faInstagramSquare,
+} from "@fortawesome/free-brands-svg-icons";
 
 const NavBar = () => {
-    return (
-        <nav>            
-            <NavLink to="/">
-              <img src={Mplogo} className="mplogo" alt="Minority Programmers Logo"/>
-            </NavLink>
-            <div className="nav_container">
-                <div className="nav_icons">
-                    <a href="https://twitter.com/minorityprogram"><FontAwesomeIcon icon={faTwitter} /></a>
-                    <a href="https://twitter.com/minorityprogram"><FontAwesomeIcon icon={faFacebookSquare} /></a>
-                    <a href="https://www.facebook.com/MinorityProgrammers"><FontAwesomeIcon icon={faLinkedin} /></a>
-                    <a href="https://www.linkedin.com/company/minority-programmers/"><FontAwesomeIcon icon={faInstagram} /></a>
-                </div>
-                <div className="nav_links">
-                    <p>Service</p>
-                    <NavLink to="/incubator">
-                        <p>Incubators</p>
-                    </NavLink>
-                    <NavLink to="/learn">
-                        <p>Learn</p>
-                    </NavLink>
-                        <p>Join</p>
-                        <p className='space'>Join</p>
-                </div>
-            </div>
-      </nav>
-    );
-  };
-  
-  export default NavBar;
+  return (
+    <nav>
+      <section className="nav_social_container">
+        <NavLink to="/">
+          <img src={Mplogo} aria-label="MPA homepage." alt="MPA Logo" />
+        </NavLink>
+        <a
+          href="https://twitter.com/minorityprogram"
+          aria-label="MPA Twitter Account."
+        >
+          <FontAwesomeIcon
+            icon={faTwitterSquare}
+            aria-hidden="true"
+            alt="Twitter Logo"
+          />
+        </a>
+        <a
+          href="https://www.facebook.com/MinorityProgrammers"
+          aria-label="MPA Facebook Account."
+        >
+          <FontAwesomeIcon
+            icon={faFacebookSquare}
+            aria-hidden="true"
+            alt="Facebook Logo"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/minority-programmers/"
+          aria-label="MPA Linkedin Account."
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            aria-hidden="true"
+            alt="Linkedin Logo"
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/minorityprogrammers/"
+          aria-label="MPA Instagram Account."
+        >
+          <FontAwesomeIcon
+            icon={faInstagramSquare}
+            aria-hidden="true"
+            alt="Instagram Logo"
+          />
+        </a>
+      </section>
+      <section className="nav_link_container">
+        <NavLink to="/incubator">
+          <p aria-label="Incubators page link.">Incubators</p>
+        </NavLink>
+        <NavLink to="/learn">
+          <p aria-label="Learn page link.">Learn</p>
+        </NavLink>
+      </section>
+    </nav>
+  );
+};
+
+export default NavBar;
